b1be01243c0ac29b468ae4ae3f2a4f46a5b7e7de
luigi-254 <jkemboi744@gmail.com>
2026-06-01 01:22:57 +0300

    Add comprehensive list of all Kenyan universities (public and private)

--- src/data/kenyanUniversities.ts
+++ src/data/kenyanUniversities.ts
@@ -5,6 +5,7 @@ export type University = {
 };
 
 export const kenyanUniversities: University[] = [
+  // Public Universities
   {
     name: "University of Nairobi",
     url: "https://www.uonbi.ac.ke",
@@ -79,6 +80,309 @@ export const kenyanUniversities: University[] = [
     ],
   },
   {
+    name: "Masinde Muliro University of Science and Technology",
+    url: "https://www.mmust.ac.ke",
+    courses: [
+      "Bachelor of Science in Nursing",
+      "Bachelor of Education (Science)",
+      "Bachelor of Science in Disaster Management",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Dedan Kimathi University of Technology",
+    url: "https://www.dkut.ac.ke",
+    courses: [
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Engineering (Mechatronic)",
+      "Bachelor of Business Administration",
+      "Bachelor of Science in Nursing",
+    ],
+  },
+  {
+    name: "Chuka University",
+    url: "https://www.chuka.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Agribusiness Management",
+      "Bachelor of Arts",
+    ],
+  },
+  {
+    name: "Technical University of Kenya",
+    url: "https://www.tukenya.ac.ke",
+    courses: [
+      "Bachelor of Engineering (Mechanical)",
+      "Bachelor of Technology in Computer Science",
+      "Bachelor of Architecture",
+      "Bachelor of Science in Media and Communication",
+    ],
+  },
+  {
+    name: "Technical University of Mombasa",
+    url: "https://www.tum.ac.ke",
+    courses: [
+      "Bachelor of Engineering (Marine)",
+      "Bachelor of Technology in Hospitality and Tourism",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Commerce",
+    ],
+  },
+  {
+    name: "Pwani University",
+    url: "https://www.pu.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Science in Marine Biology",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+    ],
+  },
+  {
+    name: "Kisii University",
+    url: "https://www.kisiiuniversity.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Nursing",
+      "Bachelor of Business Administration",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Arts",
+    ],
+  },
+  {
+    name: "University of Eldoret",
+    url: "https://www.uoeld.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Maasai Mara University",
+    url: "https://www.mmarau.ac.ke",
+    courses: [
+      "Bachelor of Science in Tourism Management",
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Wildlife Management",
+    ],
+  },
+  {
+    name: "Jaramogi Oginga Odinga University of Science and Technology",
+    url: "https://www.jooust.ac.ke",
+    courses: [
+      "Bachelor of Science in Information Technology",
+      "Bachelor of Education",
+      "Bachelor of Engineering",
+      "Bachelor of Commerce",
+    ],
+  },
+  {
+    name: "Laikipia University",
+    url: "https://www.laikipia.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+      "Bachelor of Science in Hospitality Management",
+    ],
+  },
+  {
+    name: "South Eastern Kenya University",
+    url: "https://www.seku.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Agriculture",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Meru University of Science and Technology",
+    url: "https://www.must.ac.ke",
+    courses: [
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Engineering",
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+    ],
+  },
+  {
+    name: "Multimedia University of Kenya",
+    url: "https://www.muk.ac.ke",
+    courses: [
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Business Administration",
+      "Bachelor of Arts in Media",
+      "Bachelor of Engineering in Software",
+    ],
+  },
+  {
+    name: "University of Kabianga",
+    url: "https://www.kabianga.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Agriculture",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Karatina University",
+    url: "https://www.karatina.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Agriculture",
+    ],
+  },
+  {
+    name: "Kibabii University",
+    url: "https://www.kibabii.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Engineering",
+      "Bachelor of Business Administration",
+    ],
+  },
+  {
+    name: "Rongo University",
+    url: "https://www.rongovarsity.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Arts",
+    ],
+  },
+  {
+    name: "The Co-operative University of Kenya",
+    url: "https://www.cuk.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Business Administration",
+      "Bachelor of Laws",
+    ],
+  },
+  {
+    name: "Taita Taveta University",
+    url: "https://www.ttu.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Engineering",
+      "Bachelor of Science in Environmental Management",
+    ],
+  },
+  {
+    name: "Murang'a University of Technology",
+    url: "https://www.mut.ac.ke",
+    courses: [
+      "Bachelor of Engineering",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Commerce",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "University of Embu",
+    url: "https://www.uoemu.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Machakos University",
+    url: "https://www.mksu.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Kirinyaga University",
+    url: "https://www.kyu.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Business Administration",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Garissa University",
+    url: "https://www.garissauniversity.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Arts",
+    ],
+  },
+  {
+    name: "Alupe University",
+    url: "https://www.alupe.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Science in Agriculture",
+      "Bachelor of Engineering",
+      "Bachelor of Commerce",
+    ],
+  },
+  {
+    name: "Kaimosi Friends University",
+    url: "https://www.kfu.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Nursing",
+    ],
+  },
+  {
+    name: "Tom Mboya University",
+    url: "https://www.tmu.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Information Technology",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "Tharaka University",
+    url: "https://www.tharaka.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Agriculture",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Bomet University",
+    url: "https://www.bomet.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Engineering",
+      "Bachelor of Science in Computer Science",
+    ],
+  },
+  // Private Universities
+  {
     name: "Strathmore University",
     url: "https://www.strathmore.edu",
     courses: [
@@ -98,26 +402,6 @@ export const kenyanUniversities: University[] = [
       "Bachelor of Science in Journalism",
       "Bachelor of Science in Applied Computer Technology",
       "Bachelor of Science in Psychology",
-    ],
-  },
-  {
-    name: "Technical University of Kenya",
-    url: "https://www.tukenya.ac.ke",
-    courses: [
-      "Bachelor of Engineering (Mechanical)",
-      "Bachelor of Technology in Computer Science",
-      "Bachelor of Architecture",
-      "Bachelor of Science in Media and Communication",
-    ],
-  },
-  {
-    name: "Dedan Kimathi University of Technology",
-    url: "https://www.dkut.ac.ke",
-    courses: [
-      "Bachelor of Science in Computer Science",
-      "Bachelor of Engineering (Mechatronic)",
-      "Bachelor of Business Administration",
-      "Bachelor of Science in Nursing",
     ],
   },
   {
@@ -162,54 +446,242 @@ export const kenyanUniversities: University[] = [
     ],
   },
   {
-    name: "Pwani University",
-    url: "https://www.pu.ac.ke",
+    name: "University of Eastern Africa, Baraton",
+    url: "https://www.ueab.ac.ke",
     courses: [
+      "Bachelor of Business Administration",
       "Bachelor of Education",
-      "Bachelor of Science in Marine Biology",
-      "Bachelor of Commerce",
       "Bachelor of Science in Computer Science",
+      "Bachelor of Theology",
     ],
   },
   {
-    name: "Masinde Muliro University of Science and Technology",
-    url: "https://www.mmust.ac.ke",
+    name: "Scott Christian University",
+    url: "https://www.scu.ac.ke",
     courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Business Administration",
+    ],
+  },
+  {
+    name: "Kenya Methodist University",
+    url: "https://www.kemu.ac.ke",
+    courses: [
+      "Bachelor of Medicine and Surgery",
+      "Bachelor of Education",
+      "Bachelor of Commerce",
       "Bachelor of Science in Nursing",
-      "Bachelor of Education (Science)",
-      "Bachelor of Science in Disaster Management",
+    ],
+  },
+  {
+    name: "Kabarak University",
+    url: "https://www.kabarak.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Education",
+      "Bachelor of Science in Computer Science",
       "Bachelor of Engineering",
     ],
   },
   {
-    name: "Chuka University",
-    url: "https://www.chuka.ac.ke",
+    name: "Pan Africa Christian University",
+    url: "https://www.pacu.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Education",
+      "Bachelor of Arts in Theology",
+    ],
+  },
+  {
+    name: "Zetech University",
+    url: "https://www.zetech.ac.ke",
+    courses: [
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Business Administration",
+      "Bachelor of Engineering in Software",
+      "Bachelor of Commerce",
+    ],
+  },
+  {
+    name: "St. Paul's University",
+    url: "https://www.stpaulsuniversity.ac.ke",
     courses: [
       "Bachelor of Education",
       "Bachelor of Commerce",
-      "Bachelor of Science in Agribusiness Management",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Arts in Theology",
+    ],
+  },
+  {
+    name: "Kenya Highlands Evangelical University",
+    url: "https://www.kheu.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Business Administration",
+      "Bachelor of Arts in Theology",
+    ],
+  },
+  {
+    name: "Presbyterian University of East Africa",
+    url: "https://www.puea.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Divinity",
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+    ],
+  },
+  {
+    name: "Adventist University of Africa",
+    url: "https://www.adventist.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Medicine and Surgery",
+      "Bachelor of Commerce",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "KCA University",
+    url: "https://www.kca.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Laws",
+    ],
+  },
+  {
+    name: "Gretsa University",
+    url: "https://www.gretsa.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "Great Lakes University of Kisumu",
+    url: "https://www.gluk.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Business Administration",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "Riara University",
+    url: "https://www.riara.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Business Administration",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "International Leadership University",
+    url: "https://www.iluniversity.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "Kiriri Women's University of Science & Technology",
+    url: "https://www.kiriri.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Engineering",
+    ],
+  },
+  {
+    name: "Lukenya University",
+    url: "https://www.lukenya.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "Pioneer International University",
+    url: "https://www.pioneer.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Business Administration",
+      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
+    ],
+  },
+  {
+    name: "Amref International University",
+    url: "https://www.amref.org",
+    courses: [
+      "Bachelor of Public Health",
+      "Bachelor of Health Sciences",
+      "Bachelor of Medicine",
+      "Bachelor of Nursing",
+    ],
+  },
+  {
+    name: "The East African University",
+    url: "https://www.teau.ac.ke",
+    courses: [
+      "Bachelor of Commerce",
+      "Bachelor of Business Administration",
+      "Bachelor of Education",
+      "Bachelor of Science in Computer Science",
+    ],
+  },
+  {
+    name: "Management University of Africa",
+    url: "https://www.mua.ac.ke",
+    courses: [
+      "Bachelor of Business Administration",
+      "Bachelor of Commerce",
+      "Bachelor of Laws",
+      "Bachelor of Science in Computer Science",
+    ],
+  },
+  {
+    name: "Marist International University College",
+    url: "https://www.marist.ac.ke",
+    courses: [
+      "Bachelor of Education",
+      "Bachelor of Commerce",
+      "Bachelor of Business Administration",
       "Bachelor of Arts",
     ],
   },
   {
-    name: "Maasai Mara University",
-    url: "https://www.mmarau.ac.ke",
+    name: "Islamic University of Kenya",
+    url: "https://www.iuk.ac.ke",
     courses: [
-      "Bachelor of Science in Tourism Management",
       "Bachelor of Education",
       "Bachelor of Commerce",
-      "Bachelor of Science in Wildlife Management",
+      "Bachelor of Islamic Studies",
+      "Bachelor of Science in Computer Science",
     ],
   },
   {
-    name: "Kisii University",
-    url: "https://www.kisiiuniversity.ac.ke",
+    name: "Umma University",
+    url: "https://www.umma.ac.ke",
     courses: [
-      "Bachelor of Education",
       "Bachelor of Commerce",
-      "Bachelor of Science in Nursing",
       "Bachelor of Business Administration",
-      "Bachelor of Science in Computer Science",
+      "Bachelor of Education",
       "Bachelor of Arts",
     ],
   },
